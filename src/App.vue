e2da089bee1230735152d453f839551b5b6d2aa8
azuic <zuic10a@gmail.com>
2022-03-01 22:00:34 +0800

    feat: charts for assets and links

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,21 @@
 <template>
   <h1>RSS3 Dashboard</h1>
-  <el-row>
+    <v-chart class="line-chart" :option="lineChartOptions" />
+    <el-row>
+      <el-col :span="12">
+        <v-chart class="pie-chart" :option="evmPieChartOptions" />
+      </el-col>
+      <el-col :span="12">
+        <v-chart class="pie-chart" :option="linksChartOptions" />
+        <!-- <div class="data-card">
+          <h2>
+            Links
+          </h2>
+          <span>Current Total: {{overall.links.following}}</span>
+        </div> -->
+      </el-col>
+    </el-row>
+  <!-- <el-row>
     <el-col :span="24">
       <el-card class="box-card">
         <template #header>
@@ -11,8 +26,8 @@
         <div class="text">{{ overall.count }}</div>
       </el-card>
     </el-col>
-  </el-row>
-  <el-row :gutter="20" v-for="field in (['links', 'assets'/*, 'accounts'*/])" :key="field">
+  </el-row> -->
+  <!-- <el-row :gutter="20" v-for="field in ['links', 'assets']" :key="field">
     <el-col :span="24 / (keys(overall[field]).length + 1)">
       <el-card class="box-card">
         <template #header>
@@ -23,97 +38,54 @@
         <div class="text">{{ sum(overall[field]) }}</div>
       </el-card>
     </el-col>
-    <el-col v-for="type in keys(overall[field])" :key="type" :span="24 / (keys(overall[field]).length + 1)">
+    <el-col
+      v-for="type in keys(overall[field])"
+      :key="type"
+      :span="24 / (keys(overall[field]).length + 1)"
+    >
       <el-card class="box-card">
         <template #header>
           <div class="card-header">
             <span>{{ field }}: {{ type }}</span>
           </div>
         </template>
-        <div class="text">{{ type.split('.').length === 1 ? overall[field][type] : overall[field][type.split('.')[0]][type.split('.')[1]] }}</div>
+        <div class="text">
+          {{
+            type.split(".").length === 1
+              ? overall[field][type]
+              : overall[field][type.split(".")[0]][type.split(".")[1]]
+          }}
+        </div>
       </el-card>
     </el-col>
-  </el-row>
-  <v-chart class="chart" :option="echartsOptions" />
+  </el-row> -->
+  
+
   <!-- <div class="load-details">
     <el-button type="primary" @click="loadDetails">Load Details ↓</el-button>
   </div> -->
-  <el-table
-    stripe
-    show-summary
-    :data="details"
-    style="width: 100%">
-    <el-table-column prop="address" label="Address" sortable>
-      <template #default="scope">
-        <span><a target="_black" :href="'https://rss3.io/' + scope.row.address">{{ scope.row.address }}</a></span>
-      </template>
-    </el-table-column>
-    <el-table-column prop="name" label="Name" />
-    <el-table-column prop="date_created" label="Date Created" sortable :formatter="dateFormatter" />
-    <el-table-column prop="date_updated" label="Date Updated" sortable :formatter="dateFormatter" />
-    <el-table-column v-for="field in (['links', 'assets'/*, 'accounts'*/])" :key="field" :label="field">
-      <el-table-column
-        v-for="type in Object.keys(overall[field])"
-        :key="type"
-        :label="type"
-        sortable
-        :sort-method="(a, b) => (sum(a[field][type]) || 0) - (sum(b[field][type]) || 0)"
-        :sort-orders="['descending', 'ascending', null]">
-        <template
-          v-if="field !== 'assets'"
-          #default="scope">
-          <span>{{ scope.row[field][type] || 0 }}</span>
-        </template>
-        <!-- eslint-disable -->
-        <el-table-column
-          v-if="field === 'assets'"
-          v-for="assetsType in Object.keys(overall[field][type])"
-          :key="assetsType"
-          :label="assetsType"
-          sortable
-          :sort-method="(a, b) => (a[field][type]?.[assetsType] || 0) - (b[field][type]?.[assetsType] || 0)"
-          :sort-orders="['descending', 'ascending', null]">
-          <template #default="scope">
-            <span>{{ scope.row[field][type]?.[assetsType] || 0 }}</span>
-          </template>
-        </el-table-column>
-        <!-- eslint-enable -->
-      </el-table-column>
-    </el-table-column>
-  </el-table>
 </template>
 
 <script>
 // import { nextTick } from 'vue'
-import { ElRow, ElCol, ElCard, ElTable, ElTableColumn, ElLoading } from 'element-plus'
-import axios from 'axios'
+import { ElRow, ElCol, ElLoading } from "element-plus";
+import axios from "axios";
 import { use } from "echarts/core";
 import { CanvasRenderer } from "echarts/renderers";
-import { LineChart } from "echarts/charts";
-import {
-  TooltipComponent,
-  GridComponent,
-} from "echarts/components";
+import { LineChart, PieChart } from "echarts/charts";
+import { TooltipComponent, GridComponent, PolarComponent,TitleComponent, LegendComponent } from "echarts/components";
 import VChart from "vue-echarts";
 
-use([
-  CanvasRenderer,
-  TooltipComponent,
-  LineChart,
-  GridComponent
-]);
+use([CanvasRenderer, TooltipComponent, LineChart, PieChart, GridComponent, PolarComponent,TitleComponent, LegendComponent]);
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     ElRow,
     ElCol,
-    ElCard,
-    ElTable,
-    ElTableColumn,
     VChart,
   },
-  data () {
+  data() {
     return {
       overall: {
         count: 0,
@@ -123,70 +95,186 @@ export default {
       },
       details: [],
       loading: null,
-      echartsOptions: null,
-    }
+      lineChartOptions: null,
+      evmPieChartOptions: null,
+      linksChartOptions: null
+    };
   },
   async mounted() {
-    this.openLoading()
-    this.overall = (await axios.get('https://raw.githubusercontent.com/NaturalSelectionLabs/RSS3-PreNode-Data/main/statics/overall.json')).data
+    this.openLoading();
+    this.overall = (
+      await axios.get(
+        "https://raw.githubusercontent.com/NaturalSelectionLabs/RSS3-PreNode-Data/main/statics/overall.json"
+      )
+    ).data;
 
-    const history = (await axios.get('https://raw.githubusercontent.com/NaturalSelectionLabs/RSS3-PreNode-Data/main/statics/history.json')).data
-    const historyData = Object.keys(history).map((key) => ([+new Date(key), history[key].count])).sort((a, b) => a[0] - b[0]);
-    this.echartsOptions = {
-      tooltip: {
-        trigger: 'axis',
-        axisPointer: {
-          type: 'line',
+    const history = (
+      await axios.get(
+        "https://raw.githubusercontent.com/NaturalSelectionLabs/RSS3-PreNode-Data/main/statics/history.json"
+      )
+    ).data;
+
+    const tspList = Object.keys(history);
+    var dailyMax = {};
+
+    tspList.forEach((tsp) => {
+      const currDate = tsp.split("T")[0];
+      if (Object.keys(dailyMax).includes(currDate)) {
+        if (+new Date(tsp) > +new Date(dailyMax[currDate])) {
+          dailyMax[currDate] = tsp;
         }
+      } else {
+        dailyMax[currDate] = tsp;
+      }
+    });
+
+    const historyData = Object.values(dailyMax)
+      .map((key) => [+new Date(key), history[key].count])
+      .sort((a, b) => a[0] - b[0]);
+
+    var evmData = [];
+    for (const [key, value] of Object.entries(this.overall.assets["EVM+"])) {
+      evmData.push({ value: value, name: key });
+    }    
+
+    const linksData = Object.values(dailyMax)
+      .map((key) => [+new Date(key), history[key].links.following])
+      .sort((a, b) => a[0] - b[0]);
+
+    this.lineChartOptions = {
+        title: {
+    text: 'RSS3 Files',
+    subtext: 'Current Count: '+this.overall.count,
+    left: 'center'
+  },
+      tooltip: {
+        trigger: "axis",
       },
       xAxis: {
-        name: 'Time',
-        type: 'time',
+        name: "Date",
+        type: "time",
         min: historyData[0][0],
       },
       yAxis: {
-        name: 'Count',
+        name: "",
       },
       series: [
         {
           data: historyData,
-          type: 'line',
-          smooth: true
-        }
-      ]
+          type: "line",
+          symbol: "none",
+          smooth: true,
+          lineStyle: {
+            color: "#0072ff",
+          },
+          showSymbol: false,
+        },
+      ],
     };
-  
-    this.closeLoading()
+
+    this.evmPieChartOptions = {
+              title: {
+    text: 'Assets',
+    subtext: 'Current Total: '+this.overall.assets.totalCount,
+    left: 'center'
+  },
+      tooltip: {
+        trigger: "item",
+      },
+      series: [
+        {
+          name: "EVM+ Assets",
+          type: "pie",
+                // radius: [100, 250],
+          radius: ["30%", "70%"],
+          avoidLabelOverlap: true,
+          itemStyle: {
+            borderRadius: 5,
+            borderColor: "#fff",
+            borderWidth: 1,
+          },
+      label: {
+        show:true,
+        color: 'rgba(0, 0, 0, 0.2)'
+      },
+      labelLine: {
+                lineStyle: {
+          color: 'rgba(0, 0, 0, 0.1)'
+        },
+        smooth: 0.1,
+        length:10
+      },
+          data: evmData,
+        },
+      ],
+    };
+
+    this.linksChartOptions = {
+        title: {
+    text: 'Links',
+    subtext: 'Current Total: '+this.overall.links.following,
+    left: 'center'
+  },
+      tooltip: {
+        trigger: "axis",
+      },
+      xAxis: {
+        name: "Date",
+        type: "time",
+        min: historyData[0][0],
+      },
+      yAxis: {
+        name: "",
+      },
+      series: [
+        {
+          data: linksData,
+          type: "line",
+          symbol: "none",
+          smooth: true,
+          lineStyle: {
+            color: "#0072ff",
+          },
+          showSymbol: false,
+        },
+      ],
+    };
+
+
+    this.closeLoading();
   },
   methods: {
     sum(obj) {
-      if (typeof obj !== 'object') {
-        return obj
+      if (typeof obj !== "object") {
+        return obj;
       }
-      return Object.keys(obj).reduce((p, c) => p + (typeof obj[c] === 'object' ? this.sum(obj[c]) : obj[c]), 0)
+      return Object.keys(obj).reduce(
+        (p, c) => p + (typeof obj[c] === "object" ? this.sum(obj[c]) : obj[c]),
+        0
+      );
     },
     dateFormatter(row, column, cellValue) {
-      return new Date(cellValue).toLocaleString()
+      return new Date(cellValue).toLocaleString();
     },
     keys(obj) {
       let result = [];
       for (const key in obj) {
-        if (typeof obj[key] === 'object') {
-          result = result.concat(this.keys(obj[key]).map(k => key + '.' + k))
+        if (typeof obj[key] === "object") {
+          result = result.concat(this.keys(obj[key]).map((k) => key + "." + k));
         } else {
-          result.push(key)
+          result.push(key);
         }
       }
-      return result
+      return result;
     },
     openLoading() {
       if (!this.loading) {
-        this.loading = ElLoading.service()
+        this.loading = ElLoading.service();
       }
     },
     closeLoading() {
-      this.loading.close()
-      this.loading = null
+      this.loading.close();
+      this.loading = null;
     },
     // async loadDetails() {
     //   this.openLoading()
@@ -200,11 +288,14 @@ export default {
     //   await nextTick()
     //   this.closeLoading()
     // }
-  }
-}
+  },
+};
 </script>
 
 <style>
+* {
+  font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
 h1 {
   text-align: center;
   color: rgb(0, 114, 255);
@@ -239,8 +330,19 @@ h1 {
   text-align: center;
 }
 
-.chart {
+.line-chart {
   width: 100%;
   height: 600px;
+}
+.pie-chart {
+  width: 100%;
+  height: 500px;
+}
+.data-card {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 }
 </style>
